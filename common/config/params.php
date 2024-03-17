ed105bf7401b6829fda748d77bde3594083fdfb1
xtarantulz <xxtarantulzz@gmail.com>
2024-03-17 02:22:32 +0200

    first commit

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,4 @@
 <?php
 return [
-    'user.passwordResetTokenExpire' => 3600,
-    'google_map_token' => 'AIzaSyCdCGX04wiFzzxN1dWa6ebU79CU9TEam_o',
-    'googleMapsApiKey' => 'AIzaSyCdCGX04wiFzzxN1dWa6ebU79CU9TEam_o',
-    'googleMapsLanguage' => 'uk'
+    'user.passwordResetTokenExpire' => 3600
 ];
